1985fb19797e0d9adad7d853c173b0611e83b9a2
panupind <avl.prasanna@gmail.com>
2019-06-21 16:45:25 +0530

    R Intro R Basic Data Types

---
+++ RBasics.R
@@ -0,0 +1,25 @@
+x=3
+x
+y <- x+5
+y
+m <- "Money"
+m
+class(m)
+v1 <- c(1:10)
+v1
+class(v1)
+str(v1)
+v2
+class(v2)
+str(v2)
+
+strv <- c("Introduction", "To","R")
+strv
+class(strv)
+
+logV <- c(FALSE, TRUE, TRUE, TRUE, FALSE , FALSE)
+logV
+
+class(logV)
+
+
