621ca039a2c6e88c96a42e78c82d959d2fae6e8c
panupind <avl.prasanna@gmail.com>
2019-06-25 22:21:39 +0530

    FoodNutrition V1

---
+++ FoodNutrition.R
@@ -0,0 +1,8 @@
+#1 Setwd to the appropriate Dir
+
+setwd("D:/Prasanna/Personal/Learning/Other Reads/GL/Course Material/Intro To R/RProjects/GLIntro")
+
+#2 Import Dataset to xls format
+
+FoodNutrition <- read_excel("D:/Prasanna/Personal/Learning/Other Reads/GL/Course Material/Intro To R/DataSets/Food Nutrition.xlsx")
+
