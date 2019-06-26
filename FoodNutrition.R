5d3bff09c5038692585644f9081ba48ed59fc236
panupind <avl.prasanna@gmail.com>
2019-06-26 11:04:52 +0530

    Env variable changes

--- FoodNutrition.R
+++ FoodNutrition.R
@@ -6,3 +6,29 @@ setwd("D:/Prasanna/Personal/Learning/Other Reads/GL/Course Material/Intro To R/R
 
 FoodNutrition <- read_excel("D:/Prasanna/Personal/Learning/Other Reads/GL/Course Material/Intro To R/DataSets/Food Nutrition.xlsx")
 
+#3 View top 10 rows of the Data
+
+head(FoodNutrition , 10)
+
+#4 View top 20 rows of the Data
+
+tail ( FoodNutrition , 20)
+
+#5 Show summary of the data
+
+summary(FoodNutrition)
+
+#6 Create vector using top 10 values of protein varriable
+
+attach(FoodNutrition)
+names(FoodNutrition)
+
+head(`Protein_(g)`, 10)
+
+vProt <- c(head(`Protein_(g)`, 10))
+
+vProt
+
+matxM <- matrix( data = FoodNutrition, nrow = 5 , ncol =5)
+
+matxM
